050c153378eed232ba8d4629e95fbe2d1c721052
Rezaur Rahman <rezaur.official@gmail.com>
2021-03-25 21:26:06 +0600

    Iniatial commit

---
+++ src/main/java/com/devrezaur/main/SpringBootQuizStarterApplication.java
@@ -0,0 +1,13 @@
+package com.devrezaur.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuizStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQuizStarterApplication.class, args);
+	}
+
+}
